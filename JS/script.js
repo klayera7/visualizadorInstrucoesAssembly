a036c270da4ff559544959833615bb321adb89b5
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-12 20:01:30 -0300

    fix: gerar objeto global pra manipulações

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,21 @@
 import { fecharModal } from "./modules/modalAnimado.js";
 const iframeSegmentPopup = document.querySelector("#segment_popup");
 
+const segmentValues = {}
+
 const getSegmentValues = () => {
   const iframeWindow = iframeSegmentPopup.contentWindow;
   const confirmBtn = iframeWindow.document.querySelector("#confirm_btn");
   confirmBtn.addEventListener("click", () => {
     if (iframeWindow) {
-      const dataSegment = iframeWindow.document.querySelector("#data_segment").value;
-      const extraSegment = iframeWindow.document.querySelector("#extra_segment").value;
-      const stackSegment = iframeWindow.document.querySelector("#stack_segment").value;
-      const codeSegment = iframeWindow.document.querySelector("#code_segment").value;
-      const cxCounter = iframeWindow.document.querySelector("#cx_counter").value;
+      segmentValues.dataSegment = iframeWindow.document.querySelector("#data_segment").value;
+      segmentValues.extraSegment = iframeWindow.document.querySelector("#extra_segment").value;
+      segmentValues.stackSegment = iframeWindow.document.querySelector("#stack_segment").value;
+      segmentValues.codeSegment = iframeWindow.document.querySelector("#code_segment").value;
+      segmentValues.cxCounter = iframeWindow.document.querySelector("#cx_counter").value;
       fecharModal()
-      console.log({dataSegment, extraSegment, stackSegment, codeSegment, cxCounter});
+      
+      console.log(segmentValues);
     }
   });
 };

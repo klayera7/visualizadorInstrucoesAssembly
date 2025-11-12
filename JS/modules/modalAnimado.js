a028c34663f195fb1a174643f54aa5d3d363887f
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-12 18:15:37 -0300

    fix: adicionando funcionalidade no close

--- JS/modules/modalAnimado.js
+++ JS/modules/modalAnimado.js
@@ -1,15 +1,16 @@
 const botaoAbrir = document.querySelector('[data-modal="abrir"]');
 const botaoFechar = document.querySelector('[data-modal="fechar"]');
 const containerModal = document.querySelector('[data-modal="container"]');
+const cancelarInputModal = document.querySelector('[data-modal="cancel_segment_input"]')
 
 const abrirModal = ()=>{
-  if (botaoAbrir && botaoFechar && containerModal) {
     containerModal.classList.add("ativo");
-  }
 }
 
 const fecharModal = () => {
+  const containerModal = document.querySelector('[data-modal="container"]');
   containerModal.classList.remove("ativo");
+  console.log('era pra fechar')
 }
 
 const cliqueForaModal = ()=> {
@@ -18,6 +19,22 @@ const cliqueForaModal = ()=> {
   }
 }
 
-botaoAbrir.addEventListener("click", abrirModal);
-botaoFechar.addEventListener("click", fecharModal);
-containerModal.addEventListener("click", cliqueForaModal);
+// para fechar o modal a partir do iframe (clicando no botao de cancelar)
+const fecharModalDoIframe = () => {
+    const modalNoParent = window.parent.document.querySelector('[data-modal="container"]');
+    if (modalNoParent) {
+        modalNoParent.classList.remove("ativo");
+    } else {
+        console.error("Erro: O container modal não foi encontrado na janela pai.");
+    }
+}
+
+// para evitar que os itens sejam nulos (o browser acusava erro no console)
+if (containerModal && botaoAbrir && botaoFechar){
+  botaoAbrir.addEventListener("click", abrirModal);
+  botaoFechar.addEventListener("click", fecharModal);
+  containerModal.addEventListener("click", cliqueForaModal);
+}
+
+if (cancelarInputModal) cancelarInputModal.addEventListener('click', fecharModalDoIframe)
+

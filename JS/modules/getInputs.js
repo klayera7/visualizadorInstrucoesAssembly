816b28c1db24edad34414295316929490783bb27
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-13 11:13:46 -0300

    fix: modulariazndo

---
+++ JS/modules/getInputs.js
@@ -0,0 +1,94 @@
+import { fecharModal } from "./modalAnimado.js";
+
+const iframeSegmentPopup = document.querySelector("#segment_popup");
+
+
+const segmentValues = {
+  dataSegment: "0000",
+  extraSegment: "0000",
+  stackSegment: "0000",
+  codeSegment: "0000",
+  cxCounter: "0000",
+};
+
+const segmentData = {
+  dataSegment: {
+    inputId: "data_segment",
+    registerSelector: '[data-name-segment="data"]',
+  },
+  extraSegment: {
+    inputId: "extra_segment",
+    registerSelector: '[data-name-segment="extra"]',
+  },
+  stackSegment: {
+    inputId: "stack_segment",
+    registerSelector: '[data-name-segment="stack"]',
+  },
+  codeSegment: {
+    inputId: "code_segment",
+    registerSelector: '[data-name-segment="code"]',
+  },
+  cxCounter: {
+    inputId: "cx_counter",
+    registerSelector: '[data-register="cx"]',
+  },
+};
+
+const hasDuplicates = (valueToBeAdded, object) => {
+  const normalizedValue = valueToBeAdded.trim().toUpperCase();
+  const values = Object.values(object).map((v) => v.trim().toUpperCase());
+  return values.includes(normalizedValue);
+};
+
+const loadSegmentsIntoRegisters = () => {
+  for (const key in segmentData) {
+    const selector = segmentData[key].registerSelector;
+    const registerElement = document.querySelector(selector);
+    const valorLido = segmentValues[key];
+
+    if (registerElement && valorLido !== undefined) {
+      const valorFormatado = valorLido.toUpperCase().padStart(4, "0");
+      registerElement.textContent = valorFormatado;
+    }
+  }
+};
+
+
+ const getSegmentValues = () => {
+  const iframeWindow = iframeSegmentPopup.contentWindow;
+  const confirmBtn = iframeWindow.document.querySelector("#confirm_btn");
+  if (!confirmBtn) {
+    console.log("Botão de confirmação não encontrado no iframe.");
+    return;
+  }
+  confirmBtn.addEventListener("click", () => {
+    if (iframeWindow) {
+      for (const key in segmentData) {
+        const inputId = segmentData[key].inputId;
+        const inputElement = iframeWindow.document.querySelector(`#${inputId}`);
+
+        if (inputElement) {
+          if (hasDuplicates(inputElement.value, segmentValues)) {
+            iframeWindow.alert("Os valores devem ser únicos");
+            return;
+          } else {
+            segmentValues[key] = inputElement.value;
+          }
+        }
+      }
+
+      fecharModal();
+      loadSegmentsIntoRegisters();
+      console.log("Valores dos segmentos lidos e armazenados:", segmentValues);
+    }
+  });
+};
+
+
+ export const initSegments = ()=>{
+   document.addEventListener('DOMContentLoaded', ()=>{
+     const iframe = document.querySelector('#segment_popup');
+     if (iframe) iframe.addEventListener('load', getSegmentValues)
+   })
+ }
+

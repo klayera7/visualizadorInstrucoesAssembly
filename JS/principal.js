07a8a29ec2c5b8b41074702b97692c2aa38584df
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-15 14:04:05 -0300

    rename: initSegments > logicaPopupSegmentos

--- JS/principal.js
+++ JS/principal.js
@@ -1,4 +1,4 @@
-import { initSegments } from "./modules/initSegments.js";
+import { initSegments } from "./modules/logicaPopupSegmentos.js";
 import { initModals } from "./modules/controleModal.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{

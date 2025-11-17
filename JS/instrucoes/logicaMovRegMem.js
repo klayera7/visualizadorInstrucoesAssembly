de92e00704a29009c3f2fcd4ab45930fad1d95bc
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-17 20:53:08 -0300

    fix: correcao animacao

--- JS/instrucoes/logicaMovRegMem.js
+++ JS/instrucoes/logicaMovRegMem.js
@@ -1,11 +1,11 @@
-import { escreverNoRegistrador, animarBarramentos, lerDaMemoria, lerDoRegistrador } from "../modules/simuladorUI.js";
+import { escreverNoRegistrador, animarBarramentos, lerDaMemoria, lerDoRegistrador, escreverNaMemoria } from "../modules/simuladorUI.js";
 
 
 export async function movRegMem(params) {
   const regNome = params.op1.nome; 
   const memOffset = params.op2.endereco; 
   const memValorIncial = params.op2.valorInicial
-  
+  await escreverNaMemoria('dataSegment', memOffset, memValorIncial)
   const valorDaMem = await lerDaMemoria('dataSegment', memOffset, memValorIncial)
   await escreverNoRegistrador(regNome, valorDaMem)
 }

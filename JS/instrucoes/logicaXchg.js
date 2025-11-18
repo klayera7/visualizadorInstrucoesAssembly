d2a22e35b4e565c453a97ef9bf26a90ae9d4c409
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-18 16:49:50 -0300

    fix: ajuste de conversões

--- JS/instrucoes/logicaXchg.js
+++ JS/instrucoes/logicaXchg.js
@@ -6,18 +6,15 @@ import {
   escreverNoRegistrador,
 } from "../modules/simuladorUI.js";
 
-/**
- * Executa a lógica visual da instrução XCHG (Reg, Mem)
- */
 export async function simularXCHG_Reg_Mem(params) {
   const regNome = params.op1.nome;
-  const memOffset = params.op2.endereco; // Este é o offset (ex: "5555")
-
-  // 1. Lê os valores (já vêm como números decimais)
+  const memOffset = params.op2.endereco; 
+  
+  
   const valorReg = await lerDoRegistrador(regNome, params.op1.valorInicial);
   const valorMem = await lerDaMemoria("dataSegment", memOffset, params.op2.valorInicial);
 
-  // 2. Escreve os valores trocados
+  
   await escreverNaMemoria("dataSegment", memOffset, valorReg);
   await escreverNoRegistrador(regNome, valorMem);
 }

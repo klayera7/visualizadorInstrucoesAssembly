cd0dff2b2cdaeab6df9394cf45585fbd0636cb37
Dudu2231 <eduardonascimento2231@gmail.com>
2025-11-21 18:07:45 -0300

    add: logica da instrucao ret

--- JS/instrucoes/logicaCall.js
+++ JS/instrucoes/logicaCall.js
@@ -2,28 +2,26 @@ import {
   lerDoRegistrador,
   escreverNoRegistrador,
   escreverNaMemoria,
-  animarBarramentos
+  animarBarramentos,
 } from "../modules/simuladorUI.js";
 
 export async function CALL(params) {
-
-  const destinoHex = params.op1.valor; 
+  const destinoHex = params.op1.valor;
   const destinoNum = parseInt(destinoHex, 16);
 
   const ipAtual = await lerDoRegistrador("IP", "0000");
   const ipRetorno = ipAtual + 1;
-  
+
   let spAtual = await lerDoRegistrador("SP", "0000");
 
   spAtual = spAtual - 1;
-  if (spAtual < 0) spAtual = 0xFFFF; 
+  if (spAtual < 0) spAtual = 0xffff;
 
-  
   const spHex = spAtual.toString(16).toUpperCase().padStart(4, "0");
   await escreverNaMemoria("stackSegment", spHex, ipRetorno);
 
   await escreverNoRegistrador("SP", spAtual);
- 
+
   await animarBarramentos(destinoHex.padStart(5, "0"), "CALL", 600);
   await escreverNoRegistrador("IP", destinoNum);
 }
